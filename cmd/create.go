032e0af33eb9f6958a81e0c00e17fdd908acbcae
Stenstromen <kontakt@filipstenstrom.se>
2025-02-12 10:51:45 +0100

    reduced lines with helper func

--- cmd/create.go
+++ cmd/create.go
@@ -13,16 +13,20 @@ var createCmd = &cobra.Command{
 	RunE:  resource.ResourceCreate,
 }
 
+func addCommonFlags(cmd *cobra.Command) {
+	cmd.Flags().StringP("url", "u", "", "Domain to Update (Required)")
+	cmd.Flags().StringP("subdomain", "s", "", "TLSA Subdomain (Required)")
+	cmd.Flags().StringP("cert", "f", "", "Path to Certificate File, fullchain if dane-ta is true (Required)")
+	cmd.Flags().BoolP("tcp25", "t", true, "Port 25/TCP")
+	cmd.Flags().BoolP("tcp465", "p", false, "Port 465/TCP")
+	cmd.Flags().BoolP("tcp587", "e", false, "Port 587/TCP")
+	cmd.Flags().BoolP("dane-ta", "", false, "Create DANE-TA (2 1 1) record")
+	cmd.MarkFlagRequired("url")
+	cmd.MarkFlagRequired("subdomain")
+	cmd.MarkFlagRequired("cert")
+}
+
 func init() {
 	rootCmd.AddCommand(createCmd)
-	createCmd.Flags().StringP("url", "u", "", "Domain to Update (Required)")
-	createCmd.Flags().StringP("subdomain", "s", "", "TLSA Subdomain (Required)")
-	createCmd.Flags().StringP("cert", "f", "", "Path to Certificate File, fullchain if dane-ta is true (Required)")
-	createCmd.Flags().BoolP("tcp25", "t", true, "Port 25/TCP")
-	createCmd.Flags().BoolP("tcp465", "p", false, "Port 465/TCP")
-	createCmd.Flags().BoolP("tcp587", "e", false, "Port 587/TCP")
-	createCmd.Flags().BoolP("dane-ta", "", false, "Create DANE-TA (2 1 1) record")
-	createCmd.MarkFlagRequired("url")
-	createCmd.MarkFlagRequired("subdomain")
-	createCmd.MarkFlagRequired("cert")
+	addCommonFlags(createCmd)
 }
